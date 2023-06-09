504c29f1da75b17f85d22dfdb7f8b1ff23907e95
Nhan Tri Nguyen <trinhan@evol.vn>
2023-06-09 11:42:20 +0700

    feat(community): Add more plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,18 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.project.project-nvim" },
+  { import = "astrocommunity.lsp.inc-rename-nvim" },
+
+  -- Vim motion, found at https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/motion
+  { import = "astrocommunity.motion.hop-nvim" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.nvim-spider" },
+  { import = "astrocommunity.motion.vim-matchup" },
+
+  -- Vim syntax, found at https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/syntax
+  { import = "astrocommunity.syntax.hlargs-nvim" },
+  { import = "astrocommunity.syntax.vim-cool" },
 }
