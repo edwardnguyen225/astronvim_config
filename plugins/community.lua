595c7dc469bbfe2f3e93b03672b298fd57b60a9c
Nhan Tri Nguyen <trinhan@evol.vn>
2023-06-15 10:36:06 +0700

    style: Add colorscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,12 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.lsp.inc-rename-nvim" },
