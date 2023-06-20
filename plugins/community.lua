ddfe9454d97b12447179a2bff66dabb745f307bf
Nhan Tri Nguyen <trinhan@evol.vn>
2023-06-20 16:42:47 +0700

    feat(copilot): Add copilot and its configs

--- plugins/community.lua
+++ plugins/community.lua
@@ -10,8 +10,6 @@ return {
   { import = "astrocommunity.colorscheme.everforest" },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
 
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.lsp.inc-rename-nvim" },
 
   -- Vim motion, found at https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/motion
