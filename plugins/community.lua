0c65320c6c1f998259f4c78715f1a927d2b29aee
Nhan Tri Nguyen <trinhan@evol.vn>
2023-06-26 23:01:09 +0700

    style: Move theme to another file

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,13 +3,6 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
-  { import = "astrocommunity.colorscheme.nightfox-nvim" },
-  { import = "astrocommunity.colorscheme.dracula-nvim" },
-  { import = "astrocommunity.colorscheme.everforest" },
-  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
-
   { import = "astrocommunity.lsp.inc-rename-nvim" },
 
   -- Vim motion, found at https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/motion
---
+++ plugins/theme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    enable = false,
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+}
